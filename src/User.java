8106cd809c080c69a59fdd7170921f714ce8c0d2
alexey.lavrenchuk <alexey.lavrenchuk@intexsoft.by>
2022-06-29 11:12:08 +0300

    added printlist method

--- src/User.java
+++ src/User.java
@@ -2,7 +2,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class User {
@@ -14,84 +13,89 @@ public class User {
         this.name = name;
         this.age = age;
         this.date = date;
-
     }
 
     public int getAge() {
         return age;
     }
 
-    public String getDate() {
-        return date.toString();
+    public LocalDate getDate() {
+        return date;
     }
 
     @Override
     public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
+        return name +
                 ", age=" + age +
-                ", date=" + date +
-                '}';
+                ", date=" + date;
+    }
+
+    public static int ageSum(List<User> list) {
+        int sum = list.stream().map(user -> user.getAge())
+                .reduce(Integer::sum).get();
+        return sum;
+    }
+
+    public static double avgAge(List<User> list) {
+        double avgAge = (double) ageSum(list) / list.stream().count();
+        return avgAge;
+    }
+
+    public static List<User> moreThan30(List<User> list) {
+        List<User> moreThan30list =
+                list.stream()
+                        .filter(p -> p.age > 30)
+                        .collect(Collectors.toList());
+        return moreThan30list;
+    }
+
+    public static List<User> sortedByAge(List<User> list) {
+        List<User> sortedByAge = list.stream()
+                .sorted(Comparator.comparingInt(User::getAge))
+                .collect(Collectors.toList());
+        return sortedByAge;
+    }
+
+    public static List<User> sortedByDate(List<User> list) {
+        List<User> sortedByDate = list.stream()
+                .sorted(Comparator.comparing(User::getDate))
+                .collect(Collectors.toList());
+
+        return sortedByDate;
+    }
+
+    public static void listPrint(List<User> list) {
+        list.stream().forEach(System.out::println);
     }
 
     public static void main(String[] args) {
         List<User> list = new ArrayList<>();
-        list.add(new User("Alexey", 37,LocalDate.of(2011,12,25)));
-        list.add(new User("Eugene", 39,LocalDate.of(1975,12,25)));
-        list.add(new User("Veronika", 25,LocalDate.of(2012,12,25)));
-        list.add(new User("Samuel", 45,LocalDate.of(2015,12,25)));
-        list.add(new User("Alice", 49,LocalDate.of(2000,12,25)));
-        list.add(new User("Bob", 68,LocalDate.of(2004,12,25)));
-        list.add(new User("Kate", 19,LocalDate.of(1996,12,25)));
-        list.add(new User("John", 37,LocalDate.of(1999,12,25)));
-        list.add(new User("Donald", 38,LocalDate.of(2009,12,25)));
-        list.add(new User("Benjamin", 34,LocalDate.of(2017,12,25)));
-        list.add(new User("Ryan", 24,LocalDate.of(1989,12,25)));
+        list.add(new User("Alexey", 61, LocalDate.of(2011, 12, 25)));
+        list.add(new User("Eugene", 39, LocalDate.of(1975, 12, 25)));
+        list.add(new User("Veronika", 25, LocalDate.of(2012, 12, 25)));
+        list.add(new User("Samuel", 45, LocalDate.of(2015, 12, 25)));
+        list.add(new User("Alice", 49, LocalDate.of(2000, 12, 25)));
+        list.add(new User("Bob", 68, LocalDate.of(2004, 12, 25)));
+        list.add(new User("Kate", 19, LocalDate.of(1996, 12, 25)));
+        list.add(new User("John", 37, LocalDate.of(1999, 12, 25)));
+        list.add(new User("Donald", 38, LocalDate.of(2009, 12, 25)));
+        list.add(new User("Benjamin", 34, LocalDate.of(2017, 12, 25)));
+        list.add(new User("Ryan", 24, LocalDate.of(1989, 12, 25)));
 
-AtomicInteger sum= new AtomicInteger();
-
-       list.stream()
-                .forEach(x-> sum.addAndGet(x.age));
-
-        System.out.println("Сумма всех возрастов = "+ sum);
+        System.out.println("Сумма всех возрастов сотрудников = " + ageSum(list));
         System.out.println();
-        System.out.println("Средний возраст сотруников = "+ (double)sum.get()/list.size());
-
+        System.out.printf("Средний возраст сотрудников = %.2f\n", avgAge(list));
         System.out.println();
-        System.out.println("Список сотрудников старше 30 лет ");
+        System.out.println("Список сотрудников которым > 30:");
         System.out.println();
-
-        list.stream()
-                .forEach(x-> sum.addAndGet(x.age));
-        list.stream()
-                .filter(p->p.age>30)
-                .forEach(s-> System.out.println(s));
-
+        listPrint(moreThan30(list));
         System.out.println();
-        System.out.println("Список дат приема на работу ");
+        System.out.println("Список сотрудников отсортированный по возрасту:");
         System.out.println();
-
-        list.stream()
-                .forEach(s-> System.out.println(s.date));
+        listPrint(sortedByAge(list));
         System.out.println();
-        System.out.println("Cортировка сотрудников по возрасту ");
+        System.out.println("Список сотрудников отсортированный по дате приема на работу: ");
         System.out.println();
-
-        List<User> sortedByAge= list.stream()
-                .sorted(Comparator.comparingInt(User::getAge))
-                .collect(Collectors.toList());
-
-        sortedByAge.forEach(System.out::println);
-
-        System.out.println();
-        System.out.println("Cортировка сотрудников по дате приема на работу ");
-        System.out.println();
-
-        List<User> sortedByDate= list.stream()
-                .sorted(Comparator.comparing(User::getDate))
-                .collect(Collectors.toList());
-
-        sortedByDate.forEach(System.out::println);
-
+        listPrint(sortedByDate(list));
     }
 }
